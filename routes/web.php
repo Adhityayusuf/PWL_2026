127bdd3c30ec3c4549ddd58577a36dba9af03d9a
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2026-02-26 20:39:43 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+   return 'Hello World';
+});
+Route::get('/world', function () {
+   return 'World';
+});
+Route::get('/', function () {
+   return 'Selamat Datang';
+});
+Route::get('/about', function () {
+   return 'M.Adhitya Yusuf Al-Ayyubi <br> 244107020045';
+});
+Route::get('/user/{yusuf}', function ($yusuf) {
+return 'Nama saya '.$yusuf;
+});
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+Route::get('/user/{name?}', function ($name="John") {
+return 'Nama saya '.$name;
+});
